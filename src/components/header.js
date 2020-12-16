f67413572cf1ea12fca38d350c7d3f0f2f6be270
Derrick Loo <derrickloo@hey.com>
2020-12-16 22:47:18 +0800

    Nav bar and hero adaptive layout commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import "./Header.css"
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="#">Home</Link>
+      <Link to="#">About Us</Link>
+      <Link to="#">Our Partners</Link>
+      <Link to="#">Our Services</Link>
+      <Link to="#">Contact Us</Link>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>The ancient Greek concept of hospitality</h1>
-        <p>Generosity and courtesy shown to those who are far from home and/or associates of the person bestowing guest-friendship.</p>
+        <div className="HeroContentGroup">
+          <h1>The ancient Greek concept of hospitality</h1>
+          <p>Generosity and courtesy shown to those who are far from home and/or associates of the person bestowing guest-friendship.</p>
+        </div>
         <a>Learn More</a>
       </div>
     </div>
