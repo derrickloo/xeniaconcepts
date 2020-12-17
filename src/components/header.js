dc2bc8951494a3942348293f5e2f93db555d887d
Derrick Loo <derrickloo@hey.com>
2020-12-17 12:05:51 +0800

    Keyframe animation for nav bar and hero section

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,9 @@ const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="#">Home</Link>
-      <Link to="#">About Us</Link>
-      <Link to="#">Our Partners</Link>
+      <Link to="#">Our Team</Link>
       <Link to="#">Our Services</Link>
+      <Link to="#">Our Partners</Link>
       <Link to="#">Contact Us</Link>
     </div>
   </div>
