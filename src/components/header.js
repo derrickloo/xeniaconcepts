b69da4a1453d3bef1ada0f213f463b56662e04f8
Derrick Loo <derrickloo@hey.com>
2020-12-17 17:47:01 +0800

    Contact button style change

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
     <Link to="/"><img src={require("../images/xenia-concepts-logo-white.png")} width="160" /></Link>
-      <Link to="#" className="navigation">Our Team</Link>
-      <Link to="#" className="navigation">Our Services</Link>
-      <Link to="#" className="navigation">Our Partners</Link>
-      <Link to="#" className="navigation">Contact Us</Link>
+      <Link to="#" className="navigation">About Us</Link>
+      <Link to="#" className="navigation">Services</Link>
+      <Link to="#" className="navigation">Partners</Link>
+      <Link to="#"><button>Contact</button></Link>
     </div>
   </div>
 )
