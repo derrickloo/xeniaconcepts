9cc89eaeb8a864dd0c9443e0cc8301f5d871bad2
Derrick Loo <derrickloo@hey.com>
2020-12-19 07:10:48 +0800

    Styled components for 2 version of cards; Vision section

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-    <Link to="/"><img src={require("../images/xenia-concepts-logo-white.png")} width="160" /></Link>
-      <Link to="#" className="navigation">About Us</Link>
-      <Link to="#" className="navigation">Services</Link>
-      <Link to="#" className="navigation">Partners</Link>
-      <Link to="#"><button>Contact</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+        <Link to="/"><img src={require("../images/xenia-concepts-logo-white.png")} width="160" /></Link>
+          <Link to="#" className="navigation">About Us</Link>
+          <Link to="#" className="navigation">Services</Link>
+          <Link to="#" className="navigation">Partners</Link>
+          <Link to="#"><button>Contact</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Card from '../components/Card';
+import Card2 from '../components/Card2';
 
 const IndexPage = () => (
   <div>
@@ -13,9 +14,20 @@ const IndexPage = () => (
         <a>Learn More</a>
       </div>
     </div>
+    <div className="About">
+      <div className="AboutGroup">
+        <h2>About Us</h2>
+        <h3>Our core team of industry experts and hospitality professionals are specially brought together to offer the best in class of services and expertise to meet and exceed our clients’ expectations and needs.</h3>
+      </div>
+    </div>
+    <div className="Vision">
+      <div className="VisionGroup">
+        <h3>We came together with the vision of providing advisory, asset and investment management services to hospitality owners and operators.</h3>
+      </div>
+    </div>
     <div className="Cards">
       <h2>Xenia provides comprehensive services across hospitality sectors including but not limited to:</h2>
-      <div className="CardGroup">
+      <div className="CardGroup1">
         <Card 
           title="Hotels and Resorts"
           text="• Property Planning/ Hospitality Development • Pre-Opening Planning • Hotel Valuation • Property Investments • Hospitality Crisis Management"
@@ -33,6 +45,18 @@ const IndexPage = () => (
           • Integration of (SMART) Technology and Infrastructure"
           image={require('../images/icon-airports-red.png')}
         />
+      </div>
+      <div className="CardGroup2">
+        <Card2
+            title="Wellness and Medical Centers"
+            text="• Concept Development • Sustainable Business Operations • Hospitality Consultancy • Facility Management"
+            image={require('../images/icon-wellness-red.png')}
+          />
+          <Card2
+            title="Retail businesses / F&B and Commercial Kitchens"
+            text="• Business Continuity Management • Operations Audits • Marketing and Branding • Operational and Systems Set-up • Staff Management and Training"
+            image={require('../images/icon-retail-business-red.png')}
+          />
       </div>
     </div>
   </div>
