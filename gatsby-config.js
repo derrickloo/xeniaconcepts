3fcbb8faeceeefe02f9f8bb6b339cd11f11fc3a4
Derrick Loo <derrickloo@hey.com>
2020-12-22 10:04:50 +0800

    Removed gatsby contenful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Xenia Concepts',
   },
-    plugins: ['gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        'spaceId': '3cnwnu6jet7e',
-        'accessToken': '_3EnQvt0ign65fZponcBmEgURfPOj4-Qvak3501InCU'
-      }
-    }
-  ],
+    plugins: ['gatsby-plugin-react-helmet'],
 }
